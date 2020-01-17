0ff3f695a4dfae8295efae46176807f62d6e7bc2
林晓利 <linxl1@guoji.biz>
2020-01-17 20:15:41 +0800

    finished

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import Rights from './components/Rights.vue'
 import Roles from './components/Roles.vue'
 import Cate from './components/goods/Cate.vue'
 import Params from './components/goods/Params.vue'
+import List from './components/goods/List.vue'
 
 Vue.use(Router)
 
@@ -25,7 +26,9 @@ const router = new Router({
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
-        { path: '/params', component: Params }
+        { path: '/params', component: Params },
+        { path: '/goods', component: List }
+
       ]
     }
   ]
