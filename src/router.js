c93c3f24a93987205981d70ef28881feb425096b
林晓利 <linxl1@guoji.biz>
2020-01-19 20:34:05 +0800

    完成订单列表功能开发

--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ import Cate from './components/goods/Cate.vue'
 import Params from './components/goods/Params.vue'
 import List from './components/goods/List.vue'
 import GoodAdd from './components/goods/Add.vue'
+import Order from './components/order/Order.vue'
 Vue.use(Router)
 const router = new Router({
   routes: [
@@ -27,7 +28,9 @@ const router = new Router({
         { path: '/categories', component: Cate },
         { path: '/params', component: Params },
         { path: '/goods', component: List },
-        { path: '/goods/add', component: GoodAdd }
+        { path: '/goods/add', component: GoodAdd },
+        { path: '/orders', component: Order }
+
       ]
     }
   ]
